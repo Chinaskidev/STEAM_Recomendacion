b576bf076c5ef09b2852f2c2beff37d0c4adc420
Chinaskidev <chinaskicharles0@gmail.com>
2023-10-28 13:42:33 -0300

    main modificado

--- main.py
+++ main.py
@@ -14,25 +14,29 @@ df_sentimiento_analisis= pd.read_csv('sentiment_analysis_funcion.csv', low_memor
 # Funcion def PlayTimeGenre
 
 @app.get("/release_date/{genres}")
+
 def PlayTimeGenre(genres: str):
     df_filtered = play_genre[play_genre['genres'] == genres]
 
     if df_filtered.empty:
         return {"message": f"No se encontraron datos para el género {genres}"}
 
-    usuario_año_playtime = df_filtered.groupby(['item_id', 'release_date'])['playtime_forever'].sum().reset_index()
+    max_playtime = (
+        df_filtered.groupby(['item_id', 'release_date'])
+        .agg(playtime_forever_sum=('playtime_forever', 'sum'))
+        .groupby('item_id')
+        .sum()
+        .idxmax()
+    )
 
-    max_usuario = usuario_año_playtime.groupby('item_id')['playtime_forever'].sum().idxmax()
+    df_max_playtime = df_filtered[df_filtered['item_id'] == max_playtime]
+    max_playtime_by_year = df_max_playtime.groupby('release_date')['playtime_forever'].sum()
 
-    df_max_usuario = usuario_año_playtime[usuario_año_playtime['item_id'] == max_usuario]
-
-    max_usuario_año_playtime = df_max_usuario.groupby('release_date')['playtime_forever'].sum()
-
-    max_usuario_año_playtime_list = [{"Año": str(year), "Horas": hours} for year, hours in max_usuario_año_playtime.items()]
+    max_playtime_list = [{"Año": str(year), "Horas": hours} for year, hours in max_playtime_by_year.items()]
 
     return {
-        f"Genero con más horas jugadas  {genres}": max_usuario,
-        "Horas jugadas": max_usuario_año_playtime_list
+        f"Genero con más horas jugadas {genres}": max_playtime,
+        "Horas jugadas": max_playtime_list
     }
 
 if __name__=="__main__":
@@ -42,13 +46,14 @@ if __name__=="__main__":
 @app.get("/user_genre/{genres}")
 
 def UserForGenre(genres: str):
-    
+    '''Función que devuelve al usuario con más horas jugadas para un género dado por año'''
+
     df_filtered = user_genre[user_genre['genres'] == genres]
 
     df_filtered = df_filtered[df_filtered['release_date'].str.match(r'\d{4}-\d{2}-\d{2}', na=False)]
 
     df_filtered['release_date'] = pd.to_datetime(df_filtered['release_date'], format='%Y-%m-%d')
-
+    
     user_year_playtime = df_filtered.groupby(['user_id', df_filtered['release_date'].dt.year])['playtime_forever'].sum().reset_index()
 
     max_user = user_year_playtime.groupby('user_id')['playtime_forever'].sum().idxmax()
@@ -68,16 +73,22 @@ def UserForGenre(genres: str):
 @app.get("/year")
 
 def UsersRecommend(year: int):
-    '''Devuelve los 3 juegos más recomendados por usuarios para el año dado.'''
-    
-    filtrando_reviews = user_recommend[(user_recommend['release_date'].str.contains(str(year), regex=False, na=False)) & (user_recommend['recommend'] == True)]
+    '''Devuelve los 3 juegos más recomendados por usuarios para el año dado por un usuario específico.'''
 
-    game_counts = filtrando_reviews['title'].value_counts().reset_index()
-    game_counts.columns = ['title', 'count']
+    # Filtrar reseñas recomendadas para el año y el usuario específico
+    filtered_reviews = user_recommend[(user_recommend['release_date'].str.contains(str(year), regex=False, na=False)) & (user_recommend['recommend'] == True)]
 
-    top_games = game_counts.head(3)
+    # Contar la cantidad de reseñas por título de juego y seleccionar los 3 juegos más recomendados
+    top_games = (
+        filtered_reviews['title']
+        .value_counts()
+        .head(3)
+        .reset_index()
+        .rename(columns={'index': 'title', 'title': 'count'})
+    )
 
-    top_3_games_list = [{"Puesto {}: {}".format(i+1, game): count} for i, (game, count) in enumerate(zip(top_games['title'], top_games['count']))]
+    # Crear una lista de diccionarios con los juegos más recomendados
+    top_3_games_list = [{f"Puesto {i+1}: {game}": count} for i, (game, count) in top_games.iterrows()]
 
     return top_3_games_list
 
@@ -89,19 +100,22 @@ if __name__=="__main__":
 @app.get("/año")
 
 def juegosNoRecomendados(año: int):
-    
-    ''' Y esta funcion lo que hara es lo contrario de la anterior, 
-        devolvera los juegos no recomendados por los usuarios'''
-        
-    filtrando_reviews = juegos_no_recom[(juegos_no_recom['release_date'].str.contains(str(año), regex=False, na=False)) & (juegos_no_recom['recommend'] == False)]
-    game_counts = filtrando_reviews['title'].value_counts().reset_index()
-    game_counts.columns = ['title', 'count']
-    
-    juegos_no = game_counts.head(3)
+    '''Devuelve los juegos menos recomendados por usuarios para el año dado.'''
 
-    menos_3_juegos_list = [{"Puesto {}: ".format(i+1) + game: count} for i, (game, count) in enumerate(zip(juegos_no['title'], juegos_no['count']))]
+    filtered_reviews = juegos_no_recom[(juegos_no_recom['release_date'].str.contains(str(año), regex=False, na=False)) & (juegos_no_recom['recommend'] == False)]
 
-    return menos_3_juegos_list
+    less_rated_games = (
+        filtered_reviews['title']
+        .value_counts()
+        .head(3)
+        .reset_index()
+        .rename(columns={'index': 'title', 'title': 'count'})
+    )
+
+    less_3_games_list = [{f"Puesto {i+1}: {game}": count} for i, (game, count) in less_rated_games.iterrows()]
+
+    return less_3_games_list
+
 
 if __name__=="__main__":
     uvicorn.run("main:app",port=8000,reload=True)
@@ -110,25 +124,21 @@ if __name__=="__main__":
 # Función de Sentimiento   
 @app.get("/anio")
 
-def sentiment_analysis(anio:int):
+def sentiment_analysis(anio: int):
     '''
     Función que devuelve la cantidad de registros de reseñas de usuarios 
     categorizados con un análisis de sentimiento para un año de lanzamiento específico. 
     '''
-    
+
     df_filtrado = df_sentimiento_analisis[df_sentimiento_analisis['release_date'].str.startswith(str(anio))]
 
     sentiment_counts = df_filtrado['sentiment_analysis'].value_counts()
 
-    result_dict = {"Negative": 0, "Neutral": 0, "Positive": 0}
-    
-    for index, count in sentiment_counts.items():
-        if index == 0:
-            result_dict["Negative"] = count
-        elif index == 1:
-            result_dict["Neutral"] = count
-        elif index == 2:
-            result_dict["Positive"] = count
+    result_dict = {
+        "Negative": sentiment_counts.get(0, 0),
+        "Neutral": sentiment_counts.get(1, 0),
+        "Positive": sentiment_counts.get(2, 0)
+    }
 
     return result_dict
 
